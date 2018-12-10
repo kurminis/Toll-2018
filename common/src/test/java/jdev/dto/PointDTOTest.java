e83bf717ea24b42c85a9afd07cddba5815493f46
Илья <kurmin.is@yandex.ru>
2018-12-10 23:33:58 +0700

    Преобразование из json в объект

--- common/src/test/java/jdev/dto/PointDTOTest.java
+++ common/src/test/java/jdev/dto/PointDTOTest.java
@@ -1,10 +1,15 @@
 package jdev.dto;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class PointDTOTest {
+    private String expected = "{\"lat\":56.0,\"lon\":74.0,\"autoId\":\"o567gfd\"}";
+    private String autoId = "o567gfd";
+
     @Test
     public void toJson() throws Exception {
         PointDTO point = new PointDTO();
@@ -13,5 +18,15 @@ public class PointDTOTest {
         point.setAutoId("o567gfd");
         assertTrue(point.toJson().contains("\"lat\":56"));
         System.out.println(point.toJson());
+    }
+
+    @Test
+    public void decodeDTO() throws Exception    {
+        ObjectMapper mapper = new ObjectMapper();
+        PointDTO dto = mapper.readValue(expected,PointDTO.class);
+        assertEquals(autoId,dto.getAutoId());
+        System.out.println(dto.getAutoId());
+
+
     }
 }
