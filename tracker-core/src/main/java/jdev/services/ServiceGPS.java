67fd80e1ec35fa1e063c91667657c3b964bff934
Илья <kurmin.is@yandex.ru>
2019-01-10 00:44:30 +0700

    Сервис GPS - запись в очередь

--- tracker-core/src/main/java/jdev/services/ServiceGPS.java
+++ tracker-core/src/main/java/jdev/services/ServiceGPS.java
@@ -4,8 +4,13 @@ import jdev.dto.PointDTO;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+
 @Service
 public class ServiceGPS {
+
+    private BlockingDeque<String> queue =  new LinkedBlockingDeque<>(100);
     private int count;
     private int count1;
 
@@ -17,5 +22,6 @@ public class ServiceGPS {
         point.setLon(45.0+count1++);
         point.setSpeed(65);
         System.out.println(count++ + "--"+point.toJson());
+        queue.put(point.toJson());
     }
 }
--- tracker-core/src/main/java/jdev/tracker/ShedulingContext.java
+++ tracker-core/src/main/java/jdev/tracker/ShedulingContext.java
@@ -3,7 +3,9 @@ package jdev.tracker;
 import jdev.services.ServiceGPS;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @Configuration
 @EnableScheduling
@@ -11,5 +13,13 @@ public class ShedulingContext {
     @Bean
     public ServiceGPS serviceGPS() {
         return new ServiceGPS();
+    }
+
+    @Bean
+    public TaskScheduler poolScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setThreadNamePrefix("poolScheduler");
+        scheduler.setPoolSize(20);
+        return scheduler;
     }
 }
